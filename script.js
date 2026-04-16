59ef0ac0b7f9b4d0bfd92ca5c2b39d31371d4f55
krakraigry-eng <krakraigry@gmail.com>
2026-04-16 12:27:24 +0300

    Add Lenin biography and enhance layout with dynamic content area

---
+++ script.js
@@ -0,0 +1,6 @@
+// fetch(Lenin.txt)
+//     .then(response => response.text())
+//     .then(data => {
+//         const contentDiv = document.querySelector('.dynamicContent').innerHTML=data;
+ // var iframe = document.getElementById('iframe-Lenin');
+// // iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
