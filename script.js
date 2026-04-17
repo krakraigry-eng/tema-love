9153111d302d923ea8b5efb05cdb24d79f966df3
krakraigry-eng <krakraigry@gmail.com>
2026-04-17 22:21:22 +0300

    Refactor code structure for improved readability and maintainability

---
+++ script.js
@@ -0,0 +1,13 @@
+// function OpenImage(src) {
+//     if (!src) {
+//         console.error("Ошибка: путь к изображению пустой");
+//         return;
+//          window.open(src, "_blank"); 
+//     }
+        
+// }
+// document.querySelectorAll("img").forEach(img => {
+//     img.addEventListener("click", () => {
+//         OpenImage(img.src);
+//     });
+// });
