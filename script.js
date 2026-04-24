f2dfeeec656c23c113df4b41150d5fdcba91b4a1
nitroxx079-blip <pavlenkoartom079@gmail.com>
2026-04-24 19:14:13 +0300

    Add data.json and update index.html and script.js to display dynamic content

--- script.js
+++ script.js
@@ -4,3 +4,17 @@
 //         const contentDiv = document.querySelector('.dynamicContent').innerHTML=data;
  // var iframe = document.getElementById('iframe-Lenin');
 // // iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+async function loadContent() {
+    try {
+        const response = await fetch('data.json');
+        const data = await response.json();
+
+        document.getElementById('main-title').innerText = data.title;
+        document.getElementById('site-description').innerText = data.description;
+        document.getElementById('about-section').innerText = data.about;
+    } catch (error) {
+        console.error("Помилка завантаження окремого файлу:", error);
+    }
+}
+
+loadContent();
